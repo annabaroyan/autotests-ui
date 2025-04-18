537b9df9e9085e885954cc852fd50663879eadfd
Anna Kovalenko <annabaroian@mail.ru>
2025-04-18 10:36:53 +0300

    Практика написания автотестов

--- playwright_courses.py
+++ playwright_courses.py
@@ -1,54 +1,56 @@
 from playwright.sync_api import sync_playwright, expect
 
-with sync_playwright() as playwright:
-    # Открываем браузер и создаем новую страницу
-    browser = playwright.chromium.launch(headless=False)
-    context = browser.new_context()
-    page = context.new_page()
 
-    # Переходим на страницу авторизации
-    page.goto('https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/registration')
+def courses():
+    with sync_playwright() as playwright:
+        # Открываем браузер и создаем новую страницу
+        browser = playwright.chromium.launch(headless=False)
+        context = browser.new_context()
+        page = context.new_page()
 
-    # Заполняем поля: "Email", "Username", "Password"
+        # Переходим на страницу авторизации
+        page.goto('https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/registration')
 
-    registration_email_input = page.get_by_test_id('registration-form-email-input').locator('input')
-    registration_email_input.fill('user2.name@gmail.com')
+        # Заполняем поля: "Email", "Username", "Password"
 
-    registration_username_input = page.get_by_test_id('registration-form-username-input').locator('input')
-    registration_username_input.fill('username2')
+        registration_email_input = page.get_by_test_id('registration-form-email-input').locator('input')
+        registration_email_input.fill('user2.name@gmail.com')
 
-    reg_password_input = page.get_by_test_id('registration-form-password-input').locator('input')
-    reg_password_input.fill('password123')
+        registration_username_input = page.get_by_test_id('registration-form-username-input').locator('input')
+        registration_username_input.fill('username2')
 
-    # Доп. проверка на активность кнопки "Registration"
+        reg_password_input = page.get_by_test_id('registration-form-password-input').locator('input')
+        reg_password_input.fill('password123')
 
-    registration_button = page.get_by_test_id('registration-page-registration-button')
-    expect(registration_button).to_be_enabled()
+        # Доп. проверка на активность кнопки "Registration"
 
-    # Нажимаем на кнопку "Registration", после чего произойдет редирект на страницу Dashboard
-    registration_button.click()
+        registration_button = page.get_by_test_id('registration-page-registration-button')
+        expect(registration_button).to_be_enabled()
 
-    # Проверяем, что на странице "Dashboard" отображается заголовок "Dashboard"
-    dashboard_title = page.get_by_test_id('dashboard-toolbar-title-text')
-    expect(dashboard_title).to_be_visible()
-    expect(dashboard_title).to_have_text('Dashboard')
+        # Нажимаем на кнопку "Registration", после чего произойдет редирект на страницу Dashboard
+        registration_button.click()
 
-    context.storage_state(path='browser_state.json')
+        # Проверяем, что на странице "Dashboard" отображается заголовок "Dashboard"
+        dashboard_title = page.get_by_test_id('dashboard-toolbar-title-text')
+        expect(dashboard_title).to_be_visible()
+        expect(dashboard_title).to_have_text('Dashboard')
+
+        context.storage_state(path='browser_state.json')
 
 
 
-with sync_playwright() as playwright:
-     # Открываем браузер и создаем новую страницу
-     browser = playwright.chromium.launch(headless=False)
-     context = browser.new_context(storage_state='browser_state.json')
-     page = context.new_page()
+    with sync_playwright() as playwright:
+         # Открываем браузер и создаем новую страницу
+         browser = playwright.chromium.launch(headless=False)
+         context = browser.new_context(storage_state='browser_state.json')
+         page = context.new_page()
 
-     page.goto('https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/courses')
+         page.goto('https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/courses')
 
-     courses_list_title = page.get_by_test_id('courses-list-empty-view-title-text')
-     courses_toolbar_title = page.get_by_test_id('courses-list-toolbar-title-text')
-     expect(courses_list_title).to_be_visible()
-     expect(courses_list_title).to_have_text('There is no results')
-     expect(courses_toolbar_title).to_be_visible()
-     expect(courses_toolbar_title).to_have_text('Courses')
-     page.wait_for_timeout(3000)
+         courses_list_title = page.get_by_test_id('courses-list-empty-view-title-text')
+         courses_toolbar_title = page.get_by_test_id('courses-list-toolbar-title-text')
+         expect(courses_list_title).to_be_visible()
+         expect(courses_list_title).to_have_text('There is no results')
+         expect(courses_toolbar_title).to_be_visible()
+         expect(courses_toolbar_title).to_have_text('Courses')
+         page.wait_for_timeout(3000)
---
+++ tests/test_courses.py
@@ -0,0 +1,4 @@
+from playwright_courses import courses
+
+def test_empty_courses_list():
+    courses()
