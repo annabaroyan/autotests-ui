b2ed8155fdc13caf62c07757b98ef59e0ab67fb7
Anna Kovalenko <annabaroian@mail.ru>
2025-04-19 19:09:33 +0300

    Фикстуры Pytest

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,7 +5,7 @@ from playwright.sync_api import sync_playwright, Page, Playwright
 
 
 @pytest.fixture
-def chromium_page(playwright) -> Generator[Playwright, Any, None]:
+def chromium_page(playwright: Playwright) -> Generator[Page, Any, None]:
         browser = playwright.chromium.launch(headless=False)
         yield browser.new_page()
         browser.close()
