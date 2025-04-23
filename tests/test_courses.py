917cae3562e63fbdb620b90f85f05d584f45d434
Anna Kovalenko <annabaroian@mail.ru>
2025-04-23 11:50:56 +0300

    Практикуемся в написании автотестов с использованием POM

--- tests/test_courses.py
+++ tests/test_courses.py
@@ -21,26 +21,39 @@ def test_empty_courses_list(chromium_page_with_state: Page):
 
 @pytest.mark.courses
 @pytest.mark.regression
-def test_create_course_page(chromium_page_with_state: Page, courses_list_page: CoursesListPage, create_course_page: CreateCoursePage):
-        chromium_page_with_state.goto('https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/courses/create')
+def test_create_course(courses_list_page: CoursesListPage, create_course_page: CreateCoursePage):
+        create_course_page.visit('https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/courses/create')
 
+        # Проверяем страницу создания курса
         create_course_page.check_visible_create_course_title()
         create_course_page.check_disabled_create_course_button()
         create_course_page.check_visible_image_preview_empty_view()
         create_course_page.check_visible_image_upload_view()
-        create_course_page.check_visible_create_course_form('', '', '', '0', '0')
+        create_course_page.check_visible_create_course_form(
+                title="",
+                description="",
+                estimated_time="",
+                max_score="0",
+                min_score="0"
+        )
+
+        # Проверяем блоки создания заданий
         create_course_page.check_visible_exercises_title()
         create_course_page.check_visible_create_exercise_button()
         create_course_page.check_visible_exercises_empty_view()
+
+        # Заполняем данные курса и создаем его
         create_course_page.upload_preview_image('./testdata/files/image.jpg')
-        create_course_page.check_visible_image_upload_view()
+        create_course_page.check_visible_image_upload_view(is_image_uploaded=True)
         create_course_page.fill_create_course_form('Playwright', '2 weeks', 'Playwright', '100', '10')
         create_course_page.click_create_course_button()
 
+        # Проверяем данные созданного курса
         courses_list_page.check_visible_courses_title()
         courses_list_page.check_visible_create_course_button()
-        courses_list_page.check_visible_course_card(0, 'Playwright', '100', '10', '2 weeks')
-
+        courses_list_page.check_visible_course_card(
+                index=0, title="Playwright", max_score="100", min_score="10", estimated_time="2 weeks"
+        )
 
 
 
