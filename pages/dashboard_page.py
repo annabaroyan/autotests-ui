d4605c3c03b8e5304707831b9bfd4c1aae05fd79
Anna Kovalenko <annabaroian@mail.ru>
2025-04-23 14:44:27 +0300

    Знакомство с PageComponent

--- pages/dashboard_page.py
+++ pages/dashboard_page.py
@@ -1,6 +1,7 @@
 import pytest
 from playwright.sync_api import Page, expect
 
+from components.navigation.navbar_component import NavbarComponent
 from pages.base_page import BasePage
 
 
@@ -21,6 +22,9 @@ class DashboardPage(BasePage):
 
         self.scores_title = page.get_by_test_id('scores-widget-title-text')
         self.scores_chart = page.get_by_test_id('scores-scatter-chart')
+
+        # Добавляем компонент Navbar
+        self.navbar = NavbarComponent(page)
 
 
     def check_visible_dashboard_title(self):
